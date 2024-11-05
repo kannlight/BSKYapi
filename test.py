7fab6fb09b04db94f4da93dff456a57a0383e399
kannlight <kanlight28@gmail.com>
2024-11-05 15:08:48 +0900

    修正したが出力が変

--- test.py
+++ test.py
@@ -11,4 +11,4 @@ client.login('kanlight.bsky.social', os.environ.get("pswd"))
 res = client.get_post_thread(uri='at://did:plc:qatx2fvwppss5d3qye6tpvcu/app.bsky.feed.post/3l7oysmcxqu2q')
 thread = res.thread.json()
 
-json.dumps(thread)
+print(json.dumps(thread, indent=4, sort_keys=True, ensure_ascii=False))
