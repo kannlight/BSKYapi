c5f339f5ab4780c65a1ef630476ea7dcb6d869d5
kannlight <kanlight28@gmail.com>
2024-11-05 14:43:47 +0900

    出力をjson形式に出来るか試行中

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from atproto import Client
+import json
 import os
 from dotenv import load_dotenv
 
@@ -8,6 +9,6 @@ client = Client()
 client.login('kanlight.bsky.social', os.environ.get("pswd"))
 
 res = client.get_post_thread(uri='at://did:plc:qatx2fvwppss5d3qye6tpvcu/app.bsky.feed.post/3l7oysmcxqu2q')
-thread = res.thread
+thread = res.thread.json()
 
-print(thread)
+json.dumps(thread)
