33b486728df47af08ae3b86da3ebb445d2ec91e1
kannlight <kanlight28@gmail.com>
2024-11-12 03:53:34 +0900

    collect_data()を改良中、クエリauthorが上手く使えない

--- create_dataset.py
+++ create_dataset.py
@@ -9,35 +9,31 @@ load_dotenv()
 client = Client()
 client.login('kanlight.bsky.social', os.environ.get("pswd"))
 
-def collect_data(user_did = 0, since = 0, until = 0):
-    res = {}
-    if user_did == 0:
-        p = {'q':'-http -@', 'lang':'jp', 'limit':100}
-        if since != 0:
-            p['since'] = since
-        if until != 0:
-            p['until'] = until
-        res = client.app.bsky.feed.search_posts(params=p)
-    else:
-        res = client.get_author_feed(actor=user_did, limit=100)
+def collect_data(user_did = None, since = None, until = None):
+    # 検索クエリを設定
+    p = {'q':'-http -@', 'lang':'jp', 'limit':100}
+    if user_did != None:
+        p['author'] = user_did
+    if since != None:
+        p['since'] = since
+    if until != None:
+        p['until'] = until
+    # デバッグ用
+    print(p)
+    # 検索結果を取得しjsonへ変換
+    res = client.app.bsky.feed.search_posts(params=p)
     decoded_res = json.loads(res.model_dump_json())
-    noises = []
-    if user_did == 0:
-        for post in decoded_res['posts']:
-            if post['embed'] != None:
-                noises.append(post)
-            elif post['reply_count'] == 0 and post['record']['reply'] == None:
-                noises.append(post)
-        for noise in noises:
-            decoded_res['posts'].remove(noise)
-    else:
-        for post in decoded_res['feed']:
-            if post['post']['embed'] != None:
-                noises.append(post)
-            elif post['post']['reply_count'] == 0 and post['post']['record']['reply'] == None:
-                noises.append(post)
-        for noise in noises:
-            decoded_res['feed'].remove(noise)
+
+    # 画像や動画を含む投稿やリプライの親子を持たない投稿を除外
+    # noises = []
+    # for post in decoded_res['posts']:
+    #     if post['embed'] != None:
+    #         noises.append(post)
+    #     elif post['reply_count'] == 0 and post['record']['reply'] == None:
+    #         noises.append(post)
+    # for noise in noises:
+    #     decoded_res['posts'].remove(noise)
+    
     # ファイルに書き出す
     filename = './output_collect_test/' + datetime.datetime.now().strftime('%Y%m%d_%H%M%S') + '.json'
     with open(filename, 'w') as f:
@@ -45,7 +41,7 @@ def collect_data(user_did = 0, since = 0, until = 0):
     
 def test():
     # collect_data()
-    collect_data(user_did='did:plc:ruwnuvzigdl3527oe3vjrqwj')
+    collect_data(user_did='amphoteric.bsky.social')
 
 if __name__ == "__main__":
     test()
