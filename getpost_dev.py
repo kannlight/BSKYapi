fcfc3f41042578f1ff1ce60616a9c16cf63d33b9
kannlight <kanlight28@gmail.com>
2024-11-12 03:10:18 +0900

    とりあえずcollect_data()を作った

---
+++ getpost_dev.py
@@ -0,0 +1,23 @@
+from atproto import Client
+import json
+import os
+from dotenv import load_dotenv
+import datetime
+
+# 認証
+load_dotenv()
+client = Client()
+client.login('kanlight.bsky.social', os.environ.get("pswd"))
+
+# リクエスト
+res = client.get_posts(uris=['at://did:plc:qhfo22pezo44fa3243z2h4ny/app.bsky.feed.post/3lan2p6y45s2c'])
+json_res = res.model_dump_json()
+decoded_res = json.loads(json_res)
+
+# デバッグ用出力
+print(json.dumps(decoded_res, indent=4, sort_keys=True, ensure_ascii=False))
+
+# ファイルに書き出す
+# filename = './output_test/' + datetime.datetime.now().strftime('%Y%m%d_%H%M%S') + '.json'
+# with open(filename, 'w') as f:
+#     json.dump(decoded_res, f, indent=4)
---
+++ search_test.py
@@ -0,0 +1,27 @@
+from atproto import Client
+import json
+import os
+from dotenv import load_dotenv
+import datetime
+
+# 認証
+load_dotenv()
+client = Client()
+client.login('kanlight.bsky.social', os.environ.get("pswd"))
+
+# リクエスト
+# res = client.search_posts(q='!_exists_:embed AND ')
+# res = client.app.bsky.feed.search_posts(params={'q':'-http'})
+# res = client.app.bsky.feed.search_posts(params={'q':'-http -@', 'lang':'jp'})
+res = client.get_author_feed(actor='did:plc:ojwszrpcvhwuhl4nhwactd2y')
+
+json_res = res.model_dump_json()
+decoded_res = json.loads(json_res)
+
+# デバッグ用出力
+# print(json.dumps(decoded_res, indent=4, sort_keys=True, ensure_ascii=False))
+
+# ファイルに書き出す
+filename = './output_search_test/' + datetime.datetime.now().strftime('%Y%m%d_%H%M%S') + '.json'
+with open(filename, 'w') as f:
+    json.dump(decoded_res, f, indent=4)
