be8b9c5b1c0ad4226abe4f3cc7f49d4569fe74fa
user media <kanlight28@gmail.com>
2025-01-10 14:34:11 +0900

    コードを見返してコメントを充実させた

--- create_dataset.py
+++ create_dataset.py
@@ -8,17 +8,18 @@ import pickle
 import shutil
 import time
 
-inner_data_dir = 'inner_data'
-output_collect_dir = 'output_collect'
-output_collect_author_dir = 'output_collect_author'
-creating_data_dir = 'creating_data'
-data_dir = 'data'
-poor_data_dir = 'poor_data'
-logfile = 'log/logfile'
-count = 0
-limit = 3000
-MAX_RETRIES = 5
+inner_data_dir = 'inner_data' # プログラム内部で使う変数をまとめたフォルダ
+output_collect_dir = 'output_collect' # 収集したデータを記録するフォルダ
+output_collect_author_dir = 'output_collect_author' # アカウントを指定して収集したデータを記録するフォルダ
+creating_data_dir = 'creating_data' # 作成中のデータを溜めておくフォルダ
+data_dir = 'data' # 完成したデータを保存するフォルダ
+poor_data_dir = 'poor_data' # 対話数が足りないアカウントのデータと判断されたデータを保管しておくフォルダ
+logfile = 'log/logfile' # 収集や生成の状況を書き込んでいくログデータのフォルダ
+count = 0 # 5分間のリクエスト回数
+limit = 3000 # 5分間のリクエスト回数制限
+MAX_RETRIES = 5 # インターネットが不安定な場合などリクエストに失敗した時にリトライする回数
 
+# リクエスト制限を超えた状態でリクエスト送信する前に出す例外
 class ReachedLimit(Exception):
     pass
 
@@ -36,6 +37,7 @@ for attempt in range(MAX_RETRIES):
             raise e  # リトライ上限に達したら例外をスロー
 
 def initialize():
+    # 以下３つの変数はpickleで保存して管理している。これらを初期化するための関数
     searched_trees = set()
     with open(inner_data_dir+'/searched_trees.txt','xb') as f:
         pickle.dump(searched_trees, f)
@@ -47,6 +49,7 @@ def initialize():
         pickle.dump(searched_talks, f)
 
 def collect_data(user_did = None, since = None, until = None):
+    # データを収集する関数
     global count
     if count >= limit:
         raise ReachedLimit('リクエスト制限到達')
@@ -93,6 +96,7 @@ def collect_data(user_did = None, since = None, until = None):
     return filename
 
 def create_talk(json_filename):
+    # 収集したデータから対話データを生成する関数
     global count
     # ファイルから投稿データを読み込み
     data = {}
@@ -153,8 +157,8 @@ def create_talk(json_filename):
         with open(inner_data_dir+'/searched_trees.txt','wb') as f:
             pickle.dump(searched_trees, f)
 
-# 木を根から葉への配列にばらす関数。
 def tree_to_array(tree):
+    # 木を根から葉への配列にばらす関数
     array_set = []
     # 子ノードがあるか
     if tree['replies'] != None:
@@ -172,6 +176,7 @@ def tree_to_array(tree):
     return array_set
 
 def extract_talk_from_array(array):
+    # 配列として与えられた発話の列から対話データを生成する関数
     # 探索済みの対話の集合を読み込み
     searched_talks = set()
     with open(inner_data_dir+'/searched_talks.txt','rb') as f:
@@ -241,6 +246,7 @@ def extract_talk_from_array(array):
         head = next_head
 
 def check_talk(array, head, i):
+    # 与えられた配列の中に除外条件に引っかかる発話がないかを判定する関数
     for j in range(head,i):
         # 除外条件：リンクやメンション、画像、動画を含む投稿
         if 'http' in array[j]['record']['text'] or '@' in array[j]['record']['text'] or array[j]['embed'] != None:
@@ -259,7 +265,7 @@ def check_talk(array, head, i):
 #     print(count)  
 
 def merge_data(target_f,adder_f):
-    # target_fにadder_fの対話データをマージして消去
+    # target_fにadder_fの対話データをマージして消去する関数
     target_data = {'data':[]}
     adder_data = {'data':[]}
     with open(target_f, 'r') as f:
@@ -272,34 +278,46 @@ def merge_data(target_f,adder_f):
     os.remove(adder_f)
 
 def increase_data(size_TH):
+    # 生成した対話データから、size_THを超えるように対話データを増やし、データを完成させる関数
+    # 作成中の対話データを順に処理
     for someone_file in os.listdir(creating_data_dir):
         with open(logfile, 'a') as f:
             print('request {} times'.format(count),file=f)
             print(someone_file,file=f)
+        # 対話数がsize_THを満たすか確認
         size = size_TH
         with open(creating_data_dir+'/'+someone_file, 'r') as f:
             size = len(json.load(f)['data'])
         last_time = None
         while size < size_TH:
+            # 満たない場合は同じアカウントの投稿を新たに収集して対話データを追加生成
             prev_size = size
             filename = collect_data('did:plc:'+someone_file.replace('.json',''), until=last_time)
             create_talk(filename)
+            # 対話数を更新
             with open(creating_data_dir+'/'+someone_file, 'r') as f:
                 size = len(json.load(f)['data'])
+            # 対話数が増えてない場合は対話数が満たないアカウントとみなしてやめる
             if size == prev_size:
                 break
+            # 毎回同じ対象を検索していると当然対話データは増えないので、期間をずらす
             with open(filename, 'r') as f:
                 last_time = json.load(f)['posts'][-1]['record']['created_at']
         if size < size_TH:
+            # 上記のループを経ても対話数が満たなかった場合はpoor_data_dirに保存しておく
             if os.path.exists(poor_data_dir+'/'+someone_file):
+                # 既に存在している場合、マージするとsize_THを満たす可能性がある
                 merge_data(poor_data_dir+'/'+someone_file, creating_data_dir+'/'+someone_file)
                 with open(logfile, 'a') as f:
                     print('  added {} to the poor data'.format(size),file=f)
+                # マージ後の対話数をカウント
                 poor_size = 0
                 with open(poor_data_dir+'/'+someone_file, 'r') as f:
                     poor_size = len(json.load(f))
                 if poor_size >= size_TH:
+                    # size_THを満たした場合は完成したデータとして追加
                     if os.path.exists(data_dir+'/'+someone_file):
+                        # 既に完成したデータに加えられている可能性もある。その場合はマージする。
                         merge_data(data_dir+'/'+someone_file, poor_data_dir+'/'+someone_file)
                         with open(logfile, 'a') as f:
                             print('  the poor data reached {} and merged into the data'.format(poor_size),file=f)
@@ -312,6 +330,7 @@ def increase_data(size_TH):
                 with open(logfile, 'a') as f:
                     print('  ended in {}'.format(size),file=f)
             continue
+        # ループを抜けた結果、対話数がsize_THを満たした場合、完成したデータとして追加する
         shutil.move(creating_data_dir+'/'+someone_file, data_dir)
         with open(logfile, 'a') as f:
             print('  reached {}'.format(size),file=f)
@@ -319,6 +338,7 @@ def increase_data(size_TH):
         print('request {} times'.format(count),file=f)
 
 def test2():
+    # デバッグ用、適宜書き換えてmain()の代わりに呼んで使う
     global output_collect_dir, creating_data_dir, data_dir, poor_data_dir, inner_data_dir
     output_collect_dir = 'test_data/output_collect_test2-9'
     creating_data_dir = 'test_data/creating_data_test2-9'
@@ -332,24 +352,35 @@ def test2():
     increase_data(sizeTH)
     
 def main():
+    # 新たにログファイルを用意
     global logfile
     logfile = 'log/'+datetime.datetime.now().strftime('%Y%m%d_%H%M%S')+'.txt'
+    # リクエスト回数をリセット
     count = 0
+    # 最低対話数を設定
     sizeTH = 10
+    # 初めて実行する場合は初期化が必要
     if not os.path.exists(inner_data_dir+'/searched_trees.txt'):
         initialize()
+    # 最後に収集したデータのファイルを確認
     outputs_collect = os.listdir(output_collect_dir)
     if len(outputs_collect) > 0:
         filename = output_collect_dir+'/'+sorted(outputs_collect)[-1]
     else:
+        # 無い場合（初めて実行する場合）は指定なしでデータ収集してくる
         filename = collect_data()
+    # 最後のファイルから対話データ生成（前回の続きから作業開始といったイメージ）
     create_talk(filename)
+    # 対話数を増やしたりしてデータを完成させる
     increase_data(sizeTH)
     while count < limit:
+        # リクエスト制限にかからない間繰り返す
+        # 検索期間をずらす
         with open(filename, 'r') as f:
             oldest = json.load(f)['posts'][-1]['record']['created_at']
         with open(logfile, 'a') as f:
             print('collect until {} '.format(oldest),file=f)
+        # 新たに収集、生成、作成
         filename = collect_data(until=oldest)
         create_talk(filename)
         increase_data(sizeTH)
